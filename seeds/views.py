3deeff548f06d7341bd406740e823a4d21aa1bc4
Wes <wesleySalesberry@hotmail.com>
2021-06-12 14:34:02 -0400

    added permissions

--- seeds/views.py
+++ seeds/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from .models import SeedModel
 
@@ -7,6 +8,7 @@ from .serializer import SeedSerializer
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def get_seeds(request):
     seeds = SeedModel.objects.filter(in_stock=True)
     serializer = SeedSerializer(seeds, many=True)
@@ -14,6 +16,7 @@ def get_seeds(request):
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def get_seed(request, slug):
     seeds = SeedModel.objects.get(slug=slug)
     serializer = SeedSerializer(seeds, many=False)
