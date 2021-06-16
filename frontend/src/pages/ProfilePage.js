c593ae18842a069239dbd9940daf24c2f436878a
Wes <wesleySalesberry@hotmail.com>
2021-06-16 16:44:44 -0400

    fixed input issues

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -86,21 +86,21 @@ export const ProfilePage = ({ history }) => {
                         <FormInputComponent
                             name={"firstName"}
                             label={'First Name'}
-                            placeholder={user&&user.first_name ? `Name on File ${user.first_name}` : "Enter First Name"}
+                            placeholder={user&&user.first_name ? `First Name: ${user.first_name}` : "Enter First Name"}
                             value={firstName}
                             onChange={(evt) => setFirstName(evt.target.value)}
                         />
                         <FormInputComponent
                             name={"lastName"}
                             label={'Last Name'}
-                            placeholder={user&&user.last_name ? `Name currently on File ${user.last_name}` : "Enter Last Name"}
+                            placeholder={user&&user.last_name ? `Last Name: ${user.last_name}` : "Enter Last Name"}
                             value={lastName}
                             onChange={(evt) => setLastName(evt.target.value)}
                         />
                         <FormInputComponent
                             name={"email"}
                             label={'Email'}
-                            placeholder={user&&user.email ? `Email on File ${user.email}` : "Enter Email"}
+                            placeholder={user&&user.email ? `Email: ${user.email}` : "Enter Email"}
                             value={email}
                             onChange={(evt) => setEmail(evt.target.value)}
                         />
@@ -118,35 +118,35 @@ export const ProfilePage = ({ history }) => {
                         <FormInputComponent
                             name={"street"}
                             label={'Street'}
-                            placeholder={address&&address.address ? `Street on File ${address.address}` : "Enter Street"}
+                            placeholder={address&&address.address ? `Street: ${address.address}` : "Enter Street"}
                             value={myAddress}
                             onChange={(evt) => setAddress(evt.target.value)}
                         />
                         <FormInputComponent
                             name={"city"}
                             label={'City'}
-                            placeholder={address&&address.city ? `City on File ${address.city}` : "Enter City Name"}
+                            placeholder={address&&address.city ? `City: ${address.city}` : "Enter City Name"}
                             value={city}
                             onChange={(evt) => setCity(evt.target.value)}
                         />
                         <FormInputComponent
                             name={"state"}
                             label={'State'}
-                            placeholder={address&&address.state ? `State on File ${address.state}` : "Enter Last Name"}
+                            placeholder={address&&address.state ? `State: ${address.state}` : "Enter Last Name"}
                             value={state}
                             onChange={(evt) => setState(evt.target.value)}
                         />
                         <FormInputComponent
                             name={"postalCode"}
                             label={'Postal Code'}
-                            placeholder={address&&address.postal_code ? `Postal Code on File ${address.postal_code}` : "Enter Postal Code"}
+                            placeholder={address&&address.postal_code ? `Postal: ${address.postal_code}` : "Enter Postal Code"}
                             value={postalCode}
                             onChange={(evt) => setPostalCode(evt.target.value)}
                         />
                         <FormInputComponent
                             name={"country"}
                             label={'Country'}
-                            placeholder={address&&address.country ? `Country on File ${address.country}` : "Enter Country"}
+                            placeholder={address&&address.country ? `Country: ${address.country}` : "Enter Country"}
                             value={country}
                             onChange={(evt) => setCountry(evt.target.value)}
                         />
