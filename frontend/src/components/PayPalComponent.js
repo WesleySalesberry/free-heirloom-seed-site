4f61f2df2702a262e4d99f9766c346e4ae29c095
Wes <wesleySalesberry@hotmail.com>
2021-07-02 14:18:56 -0400

    inital layout for paypal intergation

--- frontend/src/components/PayPalComponent.js
+++ frontend/src/components/PayPalComponent.js
@@ -1,21 +1,83 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom"
+
+import { Form, Button, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { FormContainer } from './FormContainer'
 
-export const PayPalComponent = ({ history }) => {
+// const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault()
-        history.push('/order')
-    }
+export const PayPalComponent = ({ total }) => {
+    const [paidFor, setPadFor ] = useState(false)
+    const [error, setError ] = useState()
+    const paypalRef = useRef()
 
+    useEffect(() => {
+        window.paypal
+            .Buttons({
+                createOrder: (data, actions) => {
+                    return actions.order.create({
+                        initent: 'CAPTURE',
+                        purchase_units: [
+                            {
+                                description: "Free Heirloom Checkout",
+                                amount:{
+                                    currency_code: 'USD',
+                                    value: 10.00,
+                                }
+                            }
+                        ]
+                    })
+                },
+
+                onApprove: async (data, actions) => {
+                    const order = await actions.order.capture();
+                    console.log(order)
+                },
+
+                onError: err => {
+                    setError(err)
+                    console.log(err)
+                }
+
+            }).render(paypalRef.current)
+    }, [total])
+    
     return (
-        <FormContainer>
-            <h3>PayPal Credit Payment</h3>
-            <Form onSubmit={handleSubmit}>
-                <p>Payment info goes here</p>
-                <Button type='submit' variant='primary'>Submit</Button>
-            </Form>
-        </FormContainer>
+        <div>
+            <ListGroup>
+                <div>Total: {total}</div>
+                <div 
+                    ref={paypalRef}
+                />
+            </ListGroup>
+        </div>
     )
 }
+
+// return (
+//         <div>
+//             <FormContainer>
+//                 <PayPalButton
+//                     createOrder={(data, actions) => createOrder(data, actions)}
+//                     onApprove={(data, actions) => onApprove(data, actions)}
+//                 />
+//             </FormContainer>
+//         </div>
+//     );
+
+
+//  const createOrder = (data, actions) => {
+        // return actions.order.create({
+        //     purchase_units: [
+        //         {
+        //             amount:{
+        //                 value: total,
+        //             }
+        //         }
+        //     ]
+        // })
+//     }
+    
+//     const  onApprove = (data, actions) => {
+//         return actions.order.capture();
+//     }
